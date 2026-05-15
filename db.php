a1c5f0d7de84b06fd91143dc6a750f8f3e553b17
jorgekagura-lgtm <jorgekagura@gmail.com>
2026-05-15 22:53:06 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,17 @@
 <?php
-$host = "localhost";
-$db   = "pop_games";
-$user = "root";
-$pass = "mysql";
-$charset = "utf8mb4";
-
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+// Datos de Clever Cloud
+$host = 'b3q2bwopggb0l4rebljf-mysql.services.clever-cloud.com';
+$db   = 'b3q2bwopggb0l4rebljf';
+$user = 'usvodfulqfnmchzk';
+$pass = 'W1TC6p5syYHXPgXdLi5k'; // Haz clic en el candado para verla
+$port = '3306';
 
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     die("Error de conexión: " . $e->getMessage());
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Si llegas aquí, ¡la conexión funciona!
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
 }
 ?>
