042d0373ef3d7741fa2ffb7833f346ef5ed55e5e
jorgekagura-lgtm <jorgekagura@gmail.com>
2026-05-15 23:24:39 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,15 @@
 <?php
-// Datos de Clever Cloud
-$host = 'b3q2bwopggb0l4rebljf-mysql.services.clever-cloud.com';
-$db   = 'b3q2bwopggb0l4rebljf';
-$user = 'usvodfulqfnmchzk';
-$pass = 'W1TC6p5syYHXPgXdLi5k'; // Haz clic en el candado para verla
+// En la nube, leemos las variables de entorno de Render
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
 $port = '3306';
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Si llegas aquí, ¡la conexión funciona!
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
