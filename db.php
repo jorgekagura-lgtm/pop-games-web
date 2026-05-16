bb2bad8a8a7d2be2c6a09f9a21d9469946f41b5f
jorgekagura-lgtm <jorgekagura@gmail.com>
2026-05-16 10:23:39 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,17 @@
 <?php
-// Credenciales con Host Externo para asegurar la conexión
+// Conexión adaptada a PostgreSQL de Render
 $host = 'dpg-cu76b65u9vis73cm4t00-a.frankfurt-postgres.render.com'; 
 $db   = 'popgames_db';
 $user = 'jorge';
 $pass = 'Uunf62DbeQeBwVpGptgT0X1K9p1N4v4m';
-$port = '3306';
+$port = '5432'; // Puerto nativo de Postgres
 
 try {
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+    // Cambiamos 'mysql:' por 'pgsql:' para que use el driver correcto
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error crítico de conexión en Render: " . $e->getMessage());
+    die("Error crítico de conexión en Render (Postgres): " . $e->getMessage());
 }
 ?>
