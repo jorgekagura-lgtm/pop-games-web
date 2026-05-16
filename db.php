1d4d3f0daf9ce2d5c4308d14bb0dc53ceab17f34
jorgekagura-lgtm <jorgekagura@gmail.com>
2026-05-16 10:16:43 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,15 @@
 <?php
-// Credenciales nativas internas de Render MySQL
-$host = 'mysql-7fbb'; 
+// Credenciales con Host Externo para asegurar la conexión
+$host = 'dpg-cu76b65u9vis73cm4t00-a.frankfurt-postgres.render.com'; 
 $db   = 'popgames_db';
 $user = 'jorge';
 $pass = 'Uunf62DbeQeBwVpGptgT0X1K9p1N4v4m';
 $port = '3306';
 
 try {
-    // Intentamos la conexión directa y limpia a MySQL
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
 } catch (PDOException $e) {
     die("Error crítico de conexión en Render: " . $e->getMessage());
 }
